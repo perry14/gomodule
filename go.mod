e8e669fa0beb66f3067e6a615759218d52af54c2
perry14 <yuezuofang@vip.qq.com>
2020-03-11 22:34:36 +0800

    go module :replace

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/perry14/gomodule
 
-require github.com/google/uuid v1.1.1
+require (
+	github.com/google/uuid v1.1.1
+	golang.org/x/text v0.3.2
+)
+
+replace (
+	github.com/google/uuid v1.1.1 => github.com/google/uuid v1.1.0
+	golang.org/x/text v0.3.2 => github.com/golang/text v0.3.2
+)
--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
 
-import "github.com/google/uuid"
+    "github.com/google/uuid"
+    "golang.org/x/text/language"
+    "golang.org/x/text/message"
+)
 
 func main() {
-	id := uuid.New().String()
-	fmt.Println("UUID:" + id)
+    id := uuid.New().String()
+    fmt.Println("UUID: ", id)
+
+    p := message.NewPrinter(language.BritishEnglish)
+    p.Printf("Number format: %v.\n", 1500)
+
+    p = message.NewPrinter(language.Greek)
+    p.Printf("Number format: %v.\n", 1500)
 }
